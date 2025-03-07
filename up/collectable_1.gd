e94280e288a853964ae88356a1a4351d4d7b1cd5
0Nott <jixiao1@s.sfusd.edu>
2025-03-07 15:06:59 -0800

    collectable added

---
+++ up/collectable_1.gd
@@ -0,0 +1,14 @@
+extends Area2D
+#this will be the game manager where scores or numbers are kept counted of
+#@onready var game_manager = ?
+
+#this will the the animation for later on use
+#animated sprite2D may replace Sprite2D since there will be mitipule frames to play
+#@onready var animation_player = ?
+
+
+#later collectable output when player interact
+#func _on_ready_entered(_body):
+#game_manager.add_point()
+#animation.player.play( name of animation )
+#https://www.freepik.com/vectors/pixel-banana
