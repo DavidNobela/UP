7e38e5aed74c5444623fbbc1f8d61c32c645232b
0Nott <jixiao1@s.sfusd.edu>
2025-03-14 15:27:02 -0700

    pause and menu

---
+++ up/pause_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.player("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.player_backward("blur")
+	
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.player("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("Pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("Pause") and get_tree().paused == true:
+		resume()
+
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_screne()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _procress(delta):
+	testEsc()
