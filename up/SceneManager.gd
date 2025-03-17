f928049a75e4435a018e9d1568347e7441947f81
0Nott <jixiao1@s.sfusd.edu>
2025-03-17 15:46:43 -0700

    -

--- up/SceneManager.gd
+++ up/SceneManager.gd
@@ -5,8 +5,18 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 
+#const Screne = preload("")
+#this is goiong to be a transition to another scene, need to be updated
+#issue with this line is that its only able to play the transition animation to the assigned transitioned scene
+#to solve: make this apply to every single transition no matter that level or situation.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
 		$Transitioning.transition()
+		
+func _on_Transitioning_transitioned():
+	pass
+	#$CurrentScene.get.child(0).queue_free()
+	#$CurrentScene.get.child(Scene.instance())
+	#print("swapped scene to?")
