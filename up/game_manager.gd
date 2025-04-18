d9dbf06870927a1fdc39ec755e3b883b5c196ecd
David <DaNobela1@s.sfusd.edu>
2025-04-17 22:11:56 -0700

    Changes

--- up/game_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var score = 0
-
-@onready var score_label = $ScoreLabel
-
-func add_point():
-	score += 1
-	score_label.text = "" #example, this could be use as your collected how many collectedables with an amount input counter connecting with the game manager
