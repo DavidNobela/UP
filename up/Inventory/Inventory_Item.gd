3e668ac84287014cf4ef98994123425c8e1119c7
0Nott <jixiao1@s.sfusd.edu>
2025-04-23 13:39:23 -0700

    -

--- up/Inventory/Inventory_Item.gd
+++ up/Inventory/Inventory_Item.gd
@@ -3,3 +3,12 @@ class_name InvItem
 
 @export var name: String = ""
 @export var texture: Texture2D
+
+
+#this is for crafting items
+#@export var recipe: Array[Item]
+#
+#@export_enum("Weapon", "Material")
+#var tpye = "Material"
+#
+#@export_multiline var description: String
