48f1503afb0a8fe230dbfba8f1149b2a45c90482
dnskvlnko <dekovalenko@s.sfusd.edu>
2025-05-05 15:49:59 -0700

    Dialogue updated

--- up/dialogue.gd
+++ up/dialogue.gd
@@ -2,9 +2,15 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 var dialogue_items: Array[String] = [
-	"Hey there",
-	"Congrats! You survived and made it out",
-	"Hopefully u didn't get too hurt",
+	"Hey, look at you! Congrats on finishing the game!",
+	"We're glad you survived and made it out of the jungle.",
+	"Now let me introduce you to the people who made this game possible.",
+	"Developers: 
+	David (coder)
+	Denys (artist)
+	Jhonatan (artist)
+	Jackson (coder)",
+	"We hope you enjoyed our game and we'll see you again!"
 ]
 
 var current_item_index := 0
@@ -15,10 +21,9 @@ func _ready() -> void:
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
-	# We animate the text appearing letter by letter.
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1
+	var text_appearing_duration := 2
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 
 
