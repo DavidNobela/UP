23ab5b6dbcc6f1a736636fbcebd1bc19b1befdfb
David <DaNobela1@s.sfusd.edu>
2025-05-18 21:58:38 -0700

    unfinished map

--- up/script/enemy.gd
+++ up/script/enemy.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
-@onready var game_manager: Node = $"../GameManager"
+@onready var game_manager: Node = $"../../GameManager"
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
