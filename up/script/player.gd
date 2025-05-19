e5bfc97a2d8e77f84c99fa4666190c823ddd64ac
David <DaNobela1@s.sfusd.edu>
2025-05-18 23:23:20 -0700

    Sound Sfx

--- up/script/player.gd
+++ up/script/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jumpsfx: AudioStreamPlayer2D = $Jumpsfx
+
+
 
 func jump():
 		velocity.y = JUMP_VELOCITY
@@ -20,6 +23,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpsfx.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
